7112f5d1c7980592ce81617d4218d2c5ba1ca529
SHIVAM RAJ <shivamteg007@gmail.com>
2025-07-20 09:53:48 +0530

    Delete BankNotes.py

--- BankNotes.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-# class which describe banknotes measurements
-
-class BankNote(BaseModel):
-    variance: float
-    skewness: float
-    curtosis: float
-    entropy: float
